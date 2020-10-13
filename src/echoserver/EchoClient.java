87cd6c7e8b441377d479c32b0e036c9e7d95e496
Cookie Vang <vang2660@morris.umn.edu>
2020-10-13 11:53:35 -0500

    Begin implementing client side

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
@@ -15,9 +17,32 @@ public class EchoClient {
 
 	private void start() throws IOException {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+		InputStream input = socket.getInputStream();
+		OutputStream output = socket.getOutputStream();
 
-		// Put your code here.
+		int nextByte; //gives the ASCII value of the characters in the input
+		char c;
+
+		while ((nextByte = System.in.read()) != -1){ //while there is input
+			c = (char)nextByte; //casting to a character so it returns what you actually input instead of your input's ASCII value
+			output.write(c); //write the byte to the output which will send to the server
+			output.flush(); //send the output to the server
+
+			System.out.write(input.read()); //server writing what it needs to send back to the client
+								//System.out.write deals with a stream of bytes
+			System.out.flush(); //flush the info from the server to the client 
+		}
+
+		input.close();
+		output.close();
+		socket.close();
 	}
+	// catch (ConnectException ce){
+	// 	System.out.println("We were unable to connect to " + "localhost");
+	// 	System.out.println("You should make sure the server is running.");
+	// }
+	// catch (IOException ioe){
+	// 	System.out.println("We caught an unexpected exception");
+	// 	System.out.println(ioe);
+	// }
 }
