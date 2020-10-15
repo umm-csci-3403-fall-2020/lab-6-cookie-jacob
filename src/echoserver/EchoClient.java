7382e47a33375ac0c83377bcc1a35a366ee6e11c
Cookie Vang <vang2660@morris.umn.edu>
2020-10-15 03:38:29 -0500

    Continue trying to implement thread on client

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,14 +1,14 @@
 package echoserver;
-
+import java.util.concurrent.atomic.AtomicInteger;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.*;
-import java.io.*;
 
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
+	private int count = 0;
+	private AtomicInteger atomicCount = new AtomicInteger(0);
 
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
@@ -17,32 +17,52 @@ public class EchoClient {
 
 	private void start() throws IOException {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream input = socket.getInputStream();
-		OutputStream output = socket.getOutputStream();
+		InputStream input = socket.getInputStream(); //input stream receiving stuff from server
+		OutputStream output = socket.getOutputStream(); //output stream sending stuff to server
 
-		int nextByte; //gives the ASCII value of the characters in the input
-		char c;
-
-		while ((nextByte = System.in.read()) != -1){ //while there is input
-			c = (char)nextByte; //casting to a character so it returns what you actually input instead of your input's ASCII value
-			output.write(c); //write the byte to the output which will send to the server
-			output.flush(); //send the output to the server
-
-			System.out.write(input.read()); //server writing what it needs to send back to the client
-								//System.out.write deals with a stream of bytes
-			System.out.flush(); //flush the info from the server to the client 
-		}
+		OutputWriter toServer = new OutputWriter(input); //
+		Thread outputThread = new Thread(toServer);
+		outputThread.start();
+		
+		InputReader result = new InputReader(output);
+		Thread inputThread = new Thread(result);
+		inputThread.start();
 
 		input.close();
 		output.close();
 		socket.close();
 	}
-	// catch (ConnectException ce){
-	// 	System.out.println("We were unable to connect to " + "localhost");
-	// 	System.out.println("You should make sure the server is running.");
-	// }
-	// catch (IOException ioe){
-	// 	System.out.println("We caught an unexpected exception");
-	// 	System.out.println(ioe);
-	// }
+	
+	public class InputReader implements Runnable{ //inputReader class
+		
+		private int inputStuff;
+		private char c;
+		public InputReader(OutputStream output) throws IOException{ //the input from keyboard will go to the output stream
+			while ((inputStuff = System.in.read()) != -1){
+				c = (char)inputStuff;
+				output.write(c);
+				output.flush();
+			}
+		}
+
+		@Override
+		public void run(){
+			//stuff
+		}
+	}
+
+	public class OutputWriter implements Runnable{
+		public int outputStuff;
+		public OutputWriter(InputStream input) throws IOException {
+			while ((outputStuff = System.in.read()) != -1){
+				System.out.write(input.read());
+				System.out.flush();
+			}
+		}
+
+		@Override 
+		public void run(){
+			//even more stuff
+		}
+	}
 }
